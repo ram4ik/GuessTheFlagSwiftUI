ad69a23ba4e96f52a27e2b402a720c5383c8d641
ram4ik <ram4ik@hotmail.com>
2023-10-12 23:42:04 -0700

    Initial Commit

---
+++ GuessTheFlagSwiftUI/GuessTheFlagSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagSwiftUIApp.swift
+//  GuessTheFlagSwiftUI
+//
+//  Created by admin on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
