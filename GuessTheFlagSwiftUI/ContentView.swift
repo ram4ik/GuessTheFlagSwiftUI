51972203a925cc5683619a758cf53f57e763b592
ram4ik <ram4ik@hotmail.com>
2023-10-13 00:07:41 -0700

    Gradient.Stop

--- GuessTheFlagSwiftUI/ContentView.swift
+++ GuessTheFlagSwiftUI/ContentView.swift
@@ -9,17 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        LinearGradient(stops: [
+            Gradient.Stop(color: .white, location: 0.45),
+            Gradient.Stop(color: .black, location: 0.55)
+        ], startPoint: .top, endPoint: .bottom)
     }
 }
 
