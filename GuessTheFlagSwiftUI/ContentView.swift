765471836cdc806ac5179d5a7d50cc4aa9876946
ram4ik <ram4ik@hotmail.com>
2023-10-13 00:04:53 -0700

    matarial

--- GuessTheFlagSwiftUI/ContentView.swift
+++ GuessTheFlagSwiftUI/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
