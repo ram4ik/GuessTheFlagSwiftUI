88086d2c51bdfb95c952ce78a7b23528f3f360cb
ram4ik <ram4ik@hotmail.com>
2023-10-13 00:09:14 -0700

    RadialGradient

--- GuessTheFlagSwiftUI/ContentView.swift
+++ GuessTheFlagSwiftUI/ContentView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LinearGradient(stops: [
-            Gradient.Stop(color: .white, location: 0.45),
-            Gradient.Stop(color: .black, location: 0.55)
-        ], startPoint: .top, endPoint: .bottom)
+        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
     }
 }
 
